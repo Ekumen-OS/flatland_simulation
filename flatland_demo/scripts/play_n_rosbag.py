b9acfb52f8ce81dedb5d6129caf4276ff2f89278
IDavGal <ignacio.davila@creativa77.com.ar>
2025-01-29 11:41:57 -0300

    Upload test rosbags
    Signed-off-by: IDavGal <ignacio.davila@creativa77.com.ar>

--- flatland_demo/scripts/play_n_rosbag.py
+++ flatland_demo/scripts/play_n_rosbag.py
@@ -16,23 +16,13 @@ if __name__=='__main__':
     number_of_robots = rospy.get_param(node_name + "/number_of_robots")
     print(flatland_demo_path)
     for i in range(0, number_of_robots):
-        if i < 5:
-            rosbagName = flatland_demo_path + "/scripts/rosbags/group_1.bag"
-        elif i < 11:
-            rosbagName = flatland_demo_path + "/scripts/rosbags/group_2.bag"
-        elif i < 17:
-            rosbagName = flatland_demo_path + "/scripts/rosbags/group_3.bag"
-        elif i < 23:
-            rosbagName = flatland_demo_path + "/scripts/rosbags/group_4.bag"
-        else:
-            robot_id_scoped = (int(i) % 3) + 1
-            rosbagName = flatland_demo_path + "/scripts/rosbags/rosbag_" + str(robot_id_scoped) + ".bag"
+        rosbagName = flatland_demo_path + "/scripts/rosbags/cmd_vel_" + str(i) + ".bag"
         print(rosbagName)
 
         if not os.path.exists(rosbagName):
             sys.stderr.write('Error: Ros bag %s not found.\n' % rosbagName)
             exit(-1)
-        rosbag_cmd = " ".join(("rosbag", "play", rosbagName, "--topics /cmd_vel /cmd_vel:=/cmd_vel_" + str(i+1)))
+        rosbag_cmd = " ".join(("rosbag", "play", rosbagName))
         print(rosbag_cmd)
 
         # Add stdout=fp for silent command, otherwise rosbags spams
